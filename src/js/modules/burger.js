0d40e1a2ffa09f6f1717a6cf11ca98db3bb47c2a
AnnaZaritskaya <annavaygunas@gmail.com>
2025-04-10 15:53:43 +0300

    Исправлен бургер убран с экранов больше MD

--- src/js/modules/burger.js
+++ src/js/modules/burger.js
@@ -1,10 +1,14 @@
 export function initBurger() {
-    const burger = document.querySelector('.burger');
-    if (burger) {
+  const burger = document.querySelector('.burger');
+  const body = document.querySelector('body'); // Предполагаю, что это элемент с классом 'head'
+
+  if (burger && body) { // Проверяем, что оба элемента существуют
       burger.addEventListener('click', function () {
-        this.classList.toggle('active');
+          this.classList.toggle('active'); // Переключаем состояние 'active' у бургера
+          body.classList.toggle('active'); // Переключаем состояние 'active' у head
       });
-    } else {
-      console.error('Элемент .burger не найден');
-    }
+  } else {
+      if (!burger) console.error('Элемент .burger не найден');
+      if (!body) console.error('Элемент body не найден');
   }
+}
